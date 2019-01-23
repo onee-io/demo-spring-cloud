a118281f510e592885d6fbd0ba81b5f39159c9ef
onee <774247851@qq.com>
2019-01-23 21:02:24 +0800

    feat(): 添加断路器监控

--- service-hi/src/main/java/me/onee/springcloud/ServiceHiApplication.java
+++ service-hi/src/main/java/me/onee/springcloud/ServiceHiApplication.java
@@ -1,10 +1,16 @@
 package me.onee.springcloud;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +24,20 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHiApplication {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
     @Bean
-    public RestTemplate getRestTemplate(){
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
@@ -34,13 +47,14 @@ public class ServiceHiApplication {
         return Sampler.ALWAYS_SAMPLE;
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServiceHiApplication.class, args);
-    }
-
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name") String name) {
         return "hi " + name + "! I am service-hi.";
+    }
+
+    public String hiError(String name) {
+        return "hi, " + name + ", sorry, error!";
     }
 
     @RequestMapping("/hello")
@@ -53,5 +67,15 @@ public class ServiceHiApplication {
         return "I'm service-hi.";
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        // 断路器监控
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
 }
