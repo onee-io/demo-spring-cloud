066b2c22eaaa2d6e3285606191c4ee642d42553a
onee <774247851@qq.com>
2019-01-23 20:32:21 +0800

    feat(): 添加zipkin调用链跟踪

--- service-miya/src/main/java/me/onee/springcloud/ServiceMiyaApplication.java
+++ service-miya/src/main/java/me/onee/springcloud/ServiceMiyaApplication.java
@@ -7,18 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by VOREVER
- * Date: 2019/1/22 21:47
+ * Date: 2019/1/23 20:20
  */
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class ServiceHiApplication {
+public class ServiceMiyaApplication {
 
     @Autowired
     private RestTemplate restTemplate;
@@ -35,23 +34,12 @@ public class ServiceHiApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceHiApplication.class, args);
+        SpringApplication.run(ServiceMiyaApplication.class, args);
     }
 
-    @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name") String name) {
-        return "hi " + name + "! I am service-hi.";
+    @RequestMapping("/miya")
+    public String callInfo() {
+        return restTemplate.getForObject("http://localhost:8081/info", String.class);
     }
-
-    @RequestMapping("/hello")
-    public String callMiya() {
-        return restTemplate.getForObject("http://localhost:8084/miya", String.class);
-    }
-
-    @RequestMapping("/info")
-    public String info() {
-        return "I'm service-hi.";
-    }
-
 
 }
