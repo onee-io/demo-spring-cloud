eb66f399f6e484e0600204e7b1fed8fe436ae878
onee <774247851@qq.com>
2019-01-23 19:35:29 +0800

    feat(): 添加 Spring Cloud Config

---
+++ config-client/src/main/java/me/onee/springcloud/ConfigClientApplication.java
@@ -0,0 +1,31 @@
+package me.onee.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by VOREVER
+ * Date: 2019/1/23 15:18
+ */
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    private String foo;
+
+    @GetMapping("/hi")
+    public String hi() {
+        return foo;
+    }
+
+}
---
+++ config-server/src/main/java/me/onee/springcloud/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package me.onee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by VOREVER
+ * Date: 2019/1/23 14:37
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
