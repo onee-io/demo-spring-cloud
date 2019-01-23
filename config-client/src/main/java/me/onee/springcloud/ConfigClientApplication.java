0eec98f518ff9aad53be799f25a8251231da53d0
onee <774247851@qq.com>
2019-01-23 19:57:44 +0800

    feat(): Spring Cloud Config 注册

--- config-client/src/main/java/me/onee/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/me/onee/springcloud/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package me.onee.springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
